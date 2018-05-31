ef8767239be9f369f8e019ae42fc07ef2694dfcb
Mathan Thava <mathan.thavachelvam@hmcts.net>
2018-05-31 16:56:38 +0100

    PR changed

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,6 @@ WORKDIR /opt/app
 
 HEALTHCHECK --interval=100s --timeout=100s --retries=10 CMD http_proxy="" wget -q http://localhost:4008/health || exit 1
 
-EXPOSE 4007
+EXPOSE 4008
 
 ENTRYPOINT ["/opt/app/bin/div-case-management-service"]
--- src/integrationTest/java/uk/gov/hmcts/reform/divorce/IntegrationTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/divorce/IntegrationTest.java
@@ -1,18 +1,14 @@
 package uk.gov.hmcts.reform.divorce;
 
-import io.restassured.response.Response;
 import net.serenitybdd.junit.spring.integration.SpringIntegrationMethodRule;
 import org.junit.Rule;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import uk.gov.hmcts.reform.authorisation.ServiceAuthorisationApi;
 import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
-
-import static net.serenitybdd.rest.SerenityRest.given;
 
 @RunWith(SpringRunner.class)
 @ContextConfiguration(classes = {ServiceContextConfiguration.class})
@@ -37,10 +33,6 @@ public abstract class IntegrationTest {
         this.springMethodIntegration = new SpringIntegrationMethodRule();
     }
 
-    Response readDataFromEvidenceManagement(String uri) {
-        getUserToken();
-        return EvidenceManagementUtil.readDataFromEvidenceManagement(uri, authTokenGenerator.generate(), "CaseWorkerTest");
-    }
 
     private synchronized String getUserToken(){
         if(userToken == null){
