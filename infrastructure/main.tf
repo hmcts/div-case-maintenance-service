17a0933cfafaedd78b45414787ae4e334456da65
Mathan Thava <mathan.thavachelvam@hmcts.net>
2018-06-08 11:24:53 +0100

    infrastructure clean up

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -21,23 +21,9 @@ module "div-cms" {
     REFORM_ENVIRONMENT                                    = "${var.env}"
     AUTH_PROVIDER_SERVICE_CLIENT_BASEURL                  = "${local.idam_s2s_url}"
     AUTH_PROVIDER_SERVICE_CLIENT_MICROSERVICE             = "${var.auth_provider_service_client_microservice}"
-    AUTH_PROVIDER_SERVICE_CLIENT_KEY                      = "${data.vault_generic_secret.div-doc-s2s-auth-secret.data["value"]}"
+    AUTH_PROVIDER_SERVICE_CLIENT_KEY                      = "${data.vault_generic_secret.ccd-submission-s2s-auth-secret.data["value"]}"
     AUTH_PROVIDER_SERVICE_CLIENT_TOKENTIMETOLIVEINSECONDS = "${var.auth_provider_service_client_tokentimetoliveinseconds}"
   }
-}
-
-# region save DB details to Azure Key Vault
-module "key-vault" {
-  source              = "git@github.com:hmcts/moj-module-key-vault?ref=master"
-  name                = "${var.product}-${var.component}-${var.env}"
-  product             = "${var.product}"
-  env                 = "${var.env}"
-  tenant_id           = "${var.tenant_id}"
-  object_id           = "${var.jenkins_AAD_objectId}"
-  resource_group_name = "${module.div-cms.resource_group_name}"
-
-  # dcd_cc-dev group object ID
-  product_group_object_id = "1c4f0704-a29e-403d-b719-b90c34ef14c9"
 }
 
 provider "vault" {
@@ -46,10 +32,4 @@ provider "vault" {
 
 data "vault_generic_secret" "ccd-submission-s2s-auth-secret" {
     path = "secret/${var.vault_env}/ccidam/service-auth-provider/api/microservice-keys/divorceCcdSubmission"
-}
-
-resource "azurerm_key_vault_secret" "ccd-submission-s2s-auth-secret" {
-    name      = "ccd-submission-s2s-auth-secret"
-    value     = "${data.vault_generic_secret.ccd-submission-s2s-auth-secret.data["value"]}"
-    vault_uri = "${module.key-vault.key_vault_uri}"
 }
