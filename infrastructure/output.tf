ee6e546468c6f5d6a8d77d98a1dd774f7d968bc8
Mathan Thava <mathan.thavachelvam@hmcts.net>
2018-08-18 15:37:21 +0100

    cleanup tf files

--- infrastructure/output.tf
+++ infrastructure/output.tf
@@ -1,3 +1,11 @@
+output "vaultUri" {
+    value = "${data.azurerm_key_vault.div_key_vault.vault_uri}"
+}
+
+output "vaultName" {
+    value = "${local.vaultName}"
+}
+
 output "auth_idam_client_baseUrl" {
     value = "${var.idam_api_baseurl}"
 }
