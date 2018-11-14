681310b980f22b8354533333cfec7e6c3d60a95c
Alex Wakeman <wakemana@gmail.com>
2018-11-14 17:25:49 +0000

    Amend rules for fetching cases for AoS / linking workflow

--- src/main/java/uk/gov/hmcts/reform/divorce/casemaintenanceservice/domain/model/CaseRetrievalStateMap.java
+++ src/main/java/uk/gov/hmcts/reform/divorce/casemaintenanceservice/domain/model/CaseRetrievalStateMap.java
@@ -32,18 +32,25 @@ public class CaseRetrievalStateMap {
                 CaseState.AOS_COMPLETED,
                 CaseState.AOS_SUBMITTED_AWAITING_ANSWER,
                 CaseState.AWAITING_CONSIDERATION_DN,
-                CaseState.AWAITING_DECREE_NISI)
+                CaseState.AWAITING_DECREE_NISI
+            )
         );
 
     public static final Map<CaseStateGrouping, List<CaseState>> RESPONDENT_CASE_STATE_GROUPING =
         ImmutableMap.of(
             CaseStateGrouping.INCOMPLETE, Collections.singletonList(CaseState.AOS_STARTED),
             CaseStateGrouping.COMPLETE, Arrays.asList(
+                CaseState.SUBMITTED,
+                CaseState.ISSUED,
+                CaseState.PENDING_REJECTION,
+                CaseState.AWAITING_CONSIDERATION_GENERAL_APPLICATION,
+                CaseState.AWAITING_DOCUMENTS,
+                CaseState.AOS_AWAITING,
+                CaseState.AOS_STARTED,
                 CaseState.AOS_COMPLETED,
                 CaseState.AOS_SUBMITTED_AWAITING_ANSWER,
-                CaseState.AWAITING_DECREE_NISI,
-                CaseState.AWAITING_LEGAL_ADVISOR_REFERRAL,
-                CaseState.DEFENDED_DIVORCE
+                CaseState.AWAITING_CONSIDERATION_DN,
+                CaseState.AWAITING_DECREE_NISI
             )
         );
 
