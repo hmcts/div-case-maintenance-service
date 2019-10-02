39dfdecaee1afeb2bd018781ffb933399780c976
Hosain <hosainnet@users.noreply.github.com>
2019-10-02 10:18:19 +0100

    Revert idam change in output.tf to fix build (#187)

--- infrastructure/output.tf
+++ infrastructure/output.tf
@@ -10,6 +10,10 @@ output "test_environment" {
   value = "${local.local_env}"
 }
 
+output "idam_api_baseurl" {
+    value = "${var.idam_api_baseurl}"
+}
+
 output "idam_s2s_url" {
   value = "${local.idam_s2s_url}"
 }
