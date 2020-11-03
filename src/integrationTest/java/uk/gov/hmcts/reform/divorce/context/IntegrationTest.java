04584fa86900ce33cacebd600369b41b900b31d0
Rob Doherty <robdohertykelly@gmail.com>
2020-11-03 15:34:47 +0000

    feature: Add test retries to the IntegrationTest class to rerun failing API tests up to a max number of tries (set at 3)

--- src/integrationTest/java/uk/gov/hmcts/reform/divorce/context/IntegrationTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/divorce/context/IntegrationTest.java
@@ -9,6 +9,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.test.context.ContextConfiguration;
+import uk.gov.hmcts.reform.divorce.RetryRule;
 import uk.gov.hmcts.reform.divorce.model.UserDetails;
 import uk.gov.hmcts.reform.divorce.support.IdamTestSupport;
 
@@ -33,6 +34,9 @@ public abstract class IntegrationTest {
 
     @Rule
     public SpringIntegrationMethodRule springMethodIntegration;
+
+    @Rule
+    public RetryRule retryRule = new RetryRule(3);
 
     protected IntegrationTest() {
         this.springMethodIntegration = new SpringIntegrationMethodRule();
