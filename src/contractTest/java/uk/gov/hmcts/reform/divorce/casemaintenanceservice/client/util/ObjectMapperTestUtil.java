7d31cebe0eb816c8d77728b41a43c95e53b28ba2
Kasi Subramaniam <Kasi.Subramaniam@HMCTS.NET>
2020-11-05 12:35:55 +0000

    DIV-COS Consumer Pact for CoreCaseDataApi.

---
+++ src/contractTest/java/uk/gov/hmcts/reform/divorce/casemaintenanceservice/client/util/ObjectMapperTestUtil.java
@@ -0,0 +1,24 @@
+package uk.gov.hmcts.reform.divorce.casemaintenanceservice.client.util;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class ObjectMapperTestUtil {
+
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+
+    public static <T> T convertStringToObject(String data, Class<T> type) {
+        try {
+            return OBJECT_MAPPER.readValue(data, type);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static String convertObjectToJsonString(final Object object) {
+        try {
+            return OBJECT_MAPPER.writeValueAsString(object);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
