2d0b9b0c2155c7fccf09b8247e73cc17be0f4877
ruban72 <ruban72@hotmail.com>
2020-12-02 10:27:47 +0000

    TA-28: Checkstyle fixes

--- src/contractTest/java/uk/gov/hmcts/reform/divorce/casemaintenanceservice/client/DivorceCaseMaintenanceSearchCases.java
+++ src/contractTest/java/uk/gov/hmcts/reform/divorce/casemaintenanceservice/client/DivorceCaseMaintenanceSearchCases.java
@@ -4,35 +4,22 @@ import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
 import au.com.dius.pact.consumer.junit5.PactTestFor;
 import au.com.dius.pact.core.model.RequestResponsePact;
 import au.com.dius.pact.core.model.annotations.Pact;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.http.client.fluent.Executor;
 import org.json.JSONException;
-import org.junit.After;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import uk.gov.hmcts.reform.ccd.client.CoreCaseDataApi;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDataContent;
-import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 import uk.gov.hmcts.reform.ccd.client.model.Event;
 import uk.gov.hmcts.reform.ccd.client.model.SearchResult;
-import uk.gov.hmcts.reform.ccd.client.model.StartEventResponse;
 import uk.gov.hmcts.reform.divorce.casemaintenanceservice.client.util.DivorceCaseMaintenancePact;
-import uk.gov.hmcts.reform.divorce.casemaintenanceservice.client.util.PactDslFixtureHelper;
 import uk.gov.hmcts.reform.divorce.casemaintenanceservice.client.util.ResourceLoader;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static uk.gov.hmcts.reform.divorce.casemaintenanceservice.client.util.AssertionHelper.assertCaseDetails;
-import static uk.gov.hmcts.reform.divorce.casemaintenanceservice.client.util.ObjectMapperTestUtil.convertObjectToJsonString;
 import static uk.gov.hmcts.reform.divorce.casemaintenanceservice.client.util.PactDslBuilderForCaseDetailsList.buildSearchResultDsl;
 
 public class DivorceCaseMaintenanceSearchCases extends DivorceCaseMaintenancePact {
@@ -63,7 +50,7 @@ public class DivorceCaseMaintenanceSearchCases extends DivorceCaseMaintenancePac
     @BeforeEach
     public void setUpEachTest() throws Exception {
         Thread.sleep(2000);
-         queryString = ResourceLoader.loadJson(VALID_QUERY);
+        queryString = ResourceLoader.loadJson(VALID_QUERY);
     }
 
     @Pact(provider = "ccdDataStoreAPI_Cases", consumer = "divorce_caseMaintenanceService")
@@ -75,10 +62,10 @@ public class DivorceCaseMaintenanceSearchCases extends DivorceCaseMaintenancePac
             .path("/searchCases")
             .query("ctid=DIVORCE")
             .method("POST")
-            .body( queryString)
+            .body(queryString)
             .headers(HttpHeaders.AUTHORIZATION, SOME_AUTHORIZATION_TOKEN, SERVICE_AUTHORIZATION,
                 SOME_SERVICE_AUTHORIZATION_TOKEN)
-            .headers(HttpHeaders.CONTENT_TYPE,MediaType.APPLICATION_JSON_VALUE)
+            .headers(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
             .matchHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
             .willRespondWith()
             .matchHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
@@ -94,8 +81,8 @@ public class DivorceCaseMaintenanceSearchCases extends DivorceCaseMaintenancePac
         SearchResult searchResult = coreCaseDataApi.searchCases(SOME_AUTHORIZATION_TOKEN,
             SOME_SERVICE_AUTHORIZATION_TOKEN, "DIVORCE", queryString);
 
-        assertEquals(searchResult.getTotal() , 123);
-        assertEquals(searchResult.getCases().size() ,1) ;
+        assertEquals(searchResult.getTotal(), 123);
+        assertEquals(searchResult.getCases().size(), 1);
 
         assertCaseDetails(searchResult.getCases().get(0)); // CaseDetail-1
     }
