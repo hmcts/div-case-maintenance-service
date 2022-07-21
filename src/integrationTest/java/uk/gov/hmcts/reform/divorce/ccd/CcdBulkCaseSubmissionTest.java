7687e84574d2050053777511e7736bff0aaebdaf
Ruban <ruban72@hotmail.com>
2022-07-21 19:39:56 +0100

    DFR-1022: Ignore tests that rely on case data being present in AAT

--- src/integrationTest/java/uk/gov/hmcts/reform/divorce/ccd/CcdBulkCaseSubmissionTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/divorce/ccd/CcdBulkCaseSubmissionTest.java
@@ -1,10 +1,12 @@
 package uk.gov.hmcts.reform.divorce.ccd;
 
 import io.restassured.response.Response;
+import org.junit.Ignore;
 import org.junit.Test;
 import uk.gov.hmcts.reform.divorce.model.UserDetails;
 import uk.gov.hmcts.reform.divorce.support.PetitionSupport;
 
+@Ignore
 public class CcdBulkCaseSubmissionTest extends PetitionSupport {
 
     private static final String CASE_PAYLOAD_PATH = "bulk-case.json";
