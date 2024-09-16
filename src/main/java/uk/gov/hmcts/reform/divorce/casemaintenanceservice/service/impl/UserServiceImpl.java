f4664bbbdf494e53687bc2e7214c084f9e6c376b
Ashley Wong <ashley.wong@justice.gov.uk>
2024-09-16 13:39:31 +0100

    remove unused import

--- src/main/java/uk/gov/hmcts/reform/divorce/casemaintenanceservice/service/impl/UserServiceImpl.java
+++ src/main/java/uk/gov/hmcts/reform/divorce/casemaintenanceservice/service/impl/UserServiceImpl.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.reform.divorce.casemaintenanceservice.service.impl;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.divorce.casemaintenanceservice.service.UserService;
